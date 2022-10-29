67f35227c94ba622cede874075c1784497924f6c
Akin Alkan <akinalkan45@gmail.com>
2022-10-29 23:17:22 +0300

    new API

--- src/test/java/get_request/Get08.java
+++ src/test/java/get_request/Get08.java
@@ -1,12 +1,21 @@
 package get_request;
 
 import base_url.JsonplaceholderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.*;
+
 public class Get08 extends JsonplaceholderBaseUrl {
+    //De-Serialization: Json datayı Java objesine çevirme
+    //Serialization: Java objesini Json formatına çevirme.
+    //De-Serialization: Iki türlü yapacağız.
+    //Gson: Google tarafından üretilmiştir.
+    //Object Mapper: Daha popüler...
     /*
         Given
            https://jsonplaceholder.typicode.com/todos/2
@@ -42,8 +51,24 @@ public class Get08 extends JsonplaceholderBaseUrl {
         expectedData.put("title", "quis ut nam facilis et officia qui");
         expectedData.put("completed", false);
         System.out.println("expectedData = " + expectedData);
+
+
         //Third Step:Send The Request and Get The Response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
         //Four Step:Do Assertion
+
+        HashMap actualData = response.as(HashMap.class);//De-Serialization
+        System.out.println("actualData = " + actualData);
+        assertEquals(expectedData.get("userId"), actualData.get("userId"));
+        assertEquals(expectedData.get("id"), actualData.get("id"));
+        assertEquals(expectedData.get("title"), actualData.get("title"));
+        assertEquals(expectedData.get("completed"), actualData.get("completed"));
+        assertEquals("1.1 vegur",response.header("Via"));
+        assertEquals("cloudflare",response.header("Server"));
+        assertEquals(200,response.statusCode());
+
 
 
     }
