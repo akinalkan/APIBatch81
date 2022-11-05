02bdd53bcd29e54536b443a6711671facc521753
Akin Alkan <akinalkan45@gmail.com>
2022-11-05 21:59:03 +0300

    API

--- src/test/java/utils/ObjectMapperUtils.java
+++ src/test/java/utils/ObjectMapperUtils.java
@@ -3,20 +3,32 @@ package utils;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ObjectMapperUtils {
-    //new ObjectMapper().readValue(jsonInString, HashMap.class)
+
+    //new ObjectMapper().readValue(jsonInString, HashMap .class);
+
     private static ObjectMapper mapper;
+
     static {
         mapper = new ObjectMapper();
     }
-    public static <T> T convertJsonToJava(String json, Class<T> cls){
-        T javaresult =null;
+
+    public static <T> T convertJsonToJava(String json, Class<T> cls){//Generic Method
+
+        T javaResult = null;
+
         try {
-           javaresult= mapper.readValue(json,cls);
+            javaResult = mapper.readValue(json,cls);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-return javaresult;
+
+        return javaResult;
     }
+
+
+
+
 }
